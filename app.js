1f3484f718623b012f068bbfb6bf0e790d17778b
Miguel <whyareyouclosed@gmail.com>
2022-03-30 09:28:42 -0500

    Add database

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('wordle api index');
+app.get('/', async (req, res) => {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * from test_table');
+    const results = JSON.stringify(result);
+    res.send(results);
 });
 
 app.listen(port, () => {
