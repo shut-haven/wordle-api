2a0ab658d88ccfa2b7e436918c06edb8140480cd
Miguel <whyareyouclosed@gmail.com>
2022-04-09 17:08:49 -0500

    fix for cors

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ const pool = new Pool({
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('/', async (req, res) => {
     const client = await pool.connect();
     const result = await client.query('SELECT * from test_table');
